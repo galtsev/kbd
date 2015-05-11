f11aad57a22ac71239114c92bd26a5647f632569
Denis Galtsev <galtsevdv@gmail.com>
2015-05-11 08:15:43 +0300

    require bootstrap

--- todo/static/todo/libs.js
+++ todo/static/todo/libs.js
@@ -2,4 +2,5 @@
 window.React = require('react');
 window.ReactBootstrap = require('react-bootstrap');
 window.jQuery = require('jquery');
+var _bootstrap = require('bootstrap');
 console.log('libs loaded', 'jQuery' in window);
